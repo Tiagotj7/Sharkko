2dbbfa09b561f1e9fcdd7806b25060e21f454d16
Tiagotj7 <tiagowork07@gmail.com>
2025-12-18 03:09:38 -0300

    edit2

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -4,7 +4,6 @@ define('BASE_PATH', dirname(__DIR__, 2));
 
 require_once BASE_PATH . '/app/config/config.php';
 require_once BASE_PATH . '/app/database/connection.php';
-require_once BASE_PATH . '/app/helpers/helpers.php';
 require_once BASE_PATH . '/app/helpers/auth.php';
 
 if (session_status() === PHP_SESSION_NONE) {
