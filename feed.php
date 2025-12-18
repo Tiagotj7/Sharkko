74f765b0c64b4b219277ee27bf579a168fdb507e
Tiagotj7 <tiagowork07@gmail.com>
2025-12-17 21:19:48 -0300

    url12

--- feed.php
+++ feed.php
@@ -3,5 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/app/controllers/PostController.php';
+require_once __DIR__ . '/app/bootstrap.php';
+require_once BASE_PATH . '/app/controllers/PostController.php';
+
 PostController::index();
