e393b103510f175292668079365b15c47f594cec
Tiagotj7 <tiagowork07@gmail.com>
2025-12-18 20:08:20 -0300

    edit9

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -1,10 +1,11 @@
 <?php
-
 define('BASE_PATH', dirname(__DIR__, 2));
 
-require_once BASE_PATH . '/app/config/config.php';
 require_once BASE_PATH . '/app/database/connection.php';
+require_once BASE_PATH . '/app/helpers/utils.php';
 require_once BASE_PATH . '/app/helpers/auth.php';
+require_once BASE_PATH . '/app/helpers/csrf.php';
+require_once BASE_PATH . '/app/helpers/validation.php';
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
