d983b678e2779cce5f57960b3b3852fd06f43795
Tiagotj7 <tiagowork07@gmail.com>
2025-12-17 22:23:35 -0300

    mv views

--- feed.php
+++ feed.php
@@ -3,5 +3,6 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/app/config/bootstrap.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/controllers/PostController.php';
 
-echo "BOOTSTRAP OK";
+echo "CONTROLLER OK";
