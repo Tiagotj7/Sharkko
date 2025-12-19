7f7ee010b29db5f87e9e917f61de11a46bbd53b4
Tiagotj7 <tiagowork07@gmail.com>
2025-12-19 01:17:26 -0300

    feed test

--- feed.php
+++ feed.php
@@ -1,14 +1,8 @@
 <?php
-// feed.php (entry point)
-
-// 1️⃣ Define BASE_PATH
 define('BASE_PATH', __DIR__ . '/app');
 
-// 2️⃣ Carrega bootstrap
 require_once BASE_PATH . '/config/bootstrap.php';
 
-// 3️⃣ (opcional) proteção de rota
 require_login();
 
-// 4️⃣ View ou controller
-require BASE_PATH . '/views/feed.php';
+require_once BASE_PATH . '/views/feed.php';
